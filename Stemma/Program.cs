d87dc0fd1d6bdb0f71b5ede795d9a01916e055fa
Geek <insooeric.son@gmail.com>
2025-03-04 19:36:32 -0500

    modified debug localhost

--- Stemma/Program.cs
+++ Stemma/Program.cs
@@ -48,7 +48,7 @@ builder.Services.AddCors(options =>
         {
             policy
                 .WithOrigins(
-                    "http://localhost:5173",
+                    "http://localhost:3000",
                     "https://stemma.vercel.app/"
                 )
                 .AllowCredentials()

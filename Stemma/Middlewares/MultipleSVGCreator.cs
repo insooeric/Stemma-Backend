4453050922e14493e693f07bf7fb319e9f707466
Geek <insooeric.son@gmail.com>
2025-04-07 20:21:47 -0400

    commented logs

--- Stemma/Middlewares/MultipleSVGCreator.cs
+++ Stemma/Middlewares/MultipleSVGCreator.cs
@@ -94,20 +94,20 @@ namespace Stemma.Middlewares
 
 
 
-            Console.WriteLine("Grid:");
-            for (int i = 0; i < numOfRow; i++)
-            {
-                for (int j = 0; j < numOfCol; j++)
-                {
-                    if (grid[i, j] != 0)
-                        Console.Write(grid[i, j] + "\t");
-                    else
-                        Console.Write("0\t");
-                }
-                Console.WriteLine();
-            }
+            //Console.WriteLine("Grid:");
+            //for (int i = 0; i < numOfRow; i++)
+            //{
+            //    for (int j = 0; j < numOfCol; j++)
+            //    {
+            //        if (grid[i, j] != 0)
+            //            Console.Write(grid[i, j] + "\t");
+            //        else
+            //            Console.Write("0\t");
+            //    }
+            //    Console.WriteLine();
+            //}
 
-            Console.WriteLine("------end------");
+            //Console.WriteLine("------end------");
 
             //double gridWidth = 0;
             //double gridHeight = 0;
